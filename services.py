97ac65c0de2403e41d41dbd2c2f257564a2cc87e
Ariel <ariell20000@hotmail.com>
2026-02-19 12:24:02 +0200

    changed parts in services.py from dicts based to db based

--- services.py
+++ services.py
@@ -1,7 +1,8 @@
-from datetime import datetime
+from sqlalchemy import func
 from fastapi import HTTPException
-
-from models import Kudos
+from database import SessionLocal
+from models_db import KudosDB
+from datetime import datetime, date
 import storage
 
 def get_leaderboard():
@@ -12,7 +13,7 @@ def get_leaderboard():
     return newleaderboard
 
 def get_kudos_by_id(kudos_id: int):
-    if (kudos_id not in storage.log):
+    if (kudos_id not in KudosDB):
         raise HTTPException(
             status_code=404,
             detail="Kudos not found. Please check the kudos ID and try again.",
@@ -44,7 +45,7 @@ def get_kudoses_by_username(username: str):
         return []
     return storage.user_log[username]
 
-def add_kudos(kudos: Kudos):
+def add_kudos(kudos):
     # give kodus
     if kudos.from_user == kudos.to_user:
         raise HTTPException( #error of http that tells the user we have an error
@@ -58,29 +59,27 @@ def add_kudos(kudos: Kudos):
             detail="Sorry, you have already gave too many kudos today. "
                    "wait for tomorrow to continue!",
         )
-    kudos.kudos_id = storage.kudos_id_counter
-    storage.kudos_id_counter+=1
-    kudos.time_created = datetime.now()
-    # Update score for the receiver
-    storage.scores[kudos.to_user] = storage.scores.get(kudos.to_user, 0) + 1
-    # update logs
-    storage.log[kudos.kudos_id]=kudos
-    storage.user_log.setdefault(kudos.to_user, []).append(kudos)
-    #echo back the kudos data
+    db = SessionLocal()
+    db_kudos = KudosDB(
+        from_user=kudos.from_user,
+        to_user=kudos.to_user,
+        message=kudos.message,
+        time_created=datetime.now()
+    )
+    db.add(db_kudos)
+    db.commit()
+    db.refresh(db_kudos)
+    db.close()
     return {
         "status": "received",
-        "from": kudos.from_user,
-        "to": kudos.to_user,
-        "message": kudos.message,
-        "kudos_id": kudos.kudos_id
+        "kudos_id": db_kudos.id
     }
-
 
 #function that checks if the user has gave too many kudos in a day, with a default limit of 5
 def check_too_many_kudos_in_day(user: str, k=5):
-    today = datetime.now().date()
-    count = 0
-    for id in storage.log:
-        if storage.log[id].from_user == user and storage.log[id].time_created.date() == today:
-            count += 1
-    return count >= k
+    db = SessionLocal()
+    kudos = db.query(KudosDB).filter(KudosDB.from_user == user, func.date(KudosDB.time_created)==date.today()).all()
+    db.close()
+    if len(kudos) >= k:
+        return True
+    return False
